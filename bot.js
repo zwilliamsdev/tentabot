75ec44278ebb6ea46b0223e80cdc56b54c0a67cd
Zachary Williams <zwilliams.dev@gmail.com>
2019-11-15 00:48:18 -0500

    Added dynamic command handler

--- bot.js
+++ bot.js
@@ -1,9 +1,25 @@
 // Discord.js module
 const Discord = require('discord.js')
-// Client object
-const client = new Discord.Client()
+// fs module for filesystem access
+const fs = require('fs')
 // Config stuffs
 const { token, prefix, status } = require('./config/config.json')
+
+// Client object
+const client = new Discord.Client()
+// Client command collection
+client.commands = new Discord.Collection()
+
+// Array of command file names
+const commandFiles = fs
+  .readdirSync('./commands')
+  .filter(file => file.endsWith('.js'))
+
+// Loop through all files and add to array of commands
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`)
+  client.commands.set(command.name, command)
+}
 
 // Once the bot is ready this will fire one time
 client.once('ready', () => {
@@ -15,8 +31,19 @@ client.once('ready', () => {
 
 // Ran whenever the bot sees a message in chat
 client.on('message', message => {
-  if (message.content === '!ping') {
-    message.channel.send('Pong!')
+  // If message does not start with the prefix or is the bot speaking end execution
+  if (!message.content.startsWith(prefix) || message.author.bot) return
+
+  const args = message.content.slice(prefix.length).split(/ +/)
+  const command = args.shift().toLowerCase()
+
+  if (!client.commands.has(command)) return
+
+  try {
+    client.commands.get(command).execute(message, args)
+  } catch (error) {
+    console.error(error)
+    message.reply('An error occured processing your command.')
   }
 })
 
