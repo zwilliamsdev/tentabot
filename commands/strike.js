34775348e648ef27308d11d7f2b128baba540fa8
Zachary Williams <zwilliams.dev@gmail.com>
2019-12-06 23:13:30 -0500

    Created embed for strikes, added todos to work on later

--- commands/strike.js
+++ commands/strike.js
@@ -1,7 +1,13 @@
+require('dotenv').config()
+const Discord = require('discord.js')
+
 module.exports = {
   name: 'strike',
   description: 'Give a user a strike',
   execute(message, args, guild) {
+    // @TODO: Check for administrative privileges and do not allow members to issue strikes
+    // @TODO: Do not allow moderators to strike other moderators
+
     // check if a user was mentioned
     if (!message.mentions.users.size)
       return message.channel.send(
@@ -17,19 +23,24 @@ module.exports = {
       return message.channel.send(
         `${message.author} you must provide a reason for the strike! Ex: strike @user reason`
       )
-    /**
-     * @TODO: FOR TESTING PURPOSES ONLY REFACTOR FOR PROD
-     **/
 
-    // Get role from server
-    const role = guild.roles.find(role => role.name === 'success')
+    // @TODO: Fix getting channel via a passed function to fix deprecation warning
+    // Get channel from server
+    // const channel = guild.channels.find(
+    //   channel => channel.name === process.env.ACTION_CHANNEL
+    // )
     const offender = message.mentions.members.first()
 
-    offender.addRole(role)
-    message.channel.send(
-      `${offender} you got a strike from ${message.author} for ${args
-        .slice(1)
-        .join(' ')}`
-    )
+    const strikeEmbed = new Discord.RichEmbed()
+      .setColor('#ff0000')
+      .setTitle('Strike Issued')
+      .setAuthor('TentaBotDev')
+      .setThumbnail('https://i.imgur.com/IvTbcrY.jpg')
+      .setDescription('A staff member has issued a strike against a member.')
+      .addField('Offending User', offender, false)
+      .addField('Reason for strike', args.slice(1).join(' '), false)
+      .addField('Struck By', message.author, false)
+
+    guild.channels.find('name', process.env.ACTION_CHANNEL).send(strikeEmbed)
   }
 }
